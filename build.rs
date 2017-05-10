a94f14200642203b05727f7b5501ead71b2f6560
David L. Inman <davidlevere@gmail.com>
2017-05-10 19:26:47 -0400

    changed curses wrapper to be dynamically linked

--- build.rs
+++ build.rs
@@ -6,17 +6,17 @@ use std::env;
 fn main() {
     let out = env::var( "OUT_DIR" ).unwrap();
 
-    Command::new("clang").args( &[ "-arch", "x86_64", "-c", "src/curses_constants.c", "-o" ] )
-                         .arg( &format!( "{}/curses_constants.o", out ) )
+    Command::new("clang").args( &[ "-arch", "x86_64", "-c", "src/curses_wrapper.c", "-o" ] )
+                         .arg( &format!( "{}/curses_wrapper.o", out ) )
                          .status()
                          .unwrap();
 
-    Command::new( "libtool" ).args( &[ "-static", "curses_constants.o", "-o", "libcurses_constants.a" ] )
+    Command::new( "libtool" ).args( &[ "-dynamic", "-lncurses", "curses_wrapper.o", "-o", "libcurses_wrapper.dylib" ] )
                              .current_dir( &Path::new( &out ) )
                              .status()
                              .unwrap();
 
     println!( "cargo:rustc-link-search=native={}", out );
-    println!( "cargo:rustc-link-lib=static=curses_constants" );
+    println!( "cargo:rustc-link-lib=dylib=curses_constants" );
 
 }
